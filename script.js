cecc9979801adf631265a2b9797f9f45c71d202a
titty91 <tznbaraldi@gmail.com>
2021-04-19 16:19:01 +0200

    impostato codice in Vue

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 function init(){
-  axios.get('https://api.themoviedb.org/3/search/movie', {
-    params: {
-                    'api_key': 'e99307154c6dfb0b4750f6603256716d',
-                    'query': 'matrix'
-                }
-            })
-         .then(console.log(data.data.resulte));
-         .catch(console.log('error');)
+  new Vue({
+    el:'#app',
+    data: {
+      query: '',
+      listaFilm:[]
+    },
+    mounted(){
+
+    },
+    methods:{
+      cercaClick(){
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+                      'api_key': 'e99307154c6dfb0b4750f6603256716d',
+                      'query': this.query,
+                      'language': 'it-IT'
+                    }
+                  })
+               .then(function(respons){
+                 console.log(respons.data.results);
+                 this.listaFilm = respons.data.results
+                 console.log(this.listaFilm);
+               })
+               .catch(function(){
+                 console.log('error');
+               })
+
+      }
+    }
+  })
+
 }
 
 init();
