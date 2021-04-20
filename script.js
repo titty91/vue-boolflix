0f7ab064c138394de28f7f9244f4475e71cad502
titty91 <tznbaraldi@gmail.com>
2021-04-20 16:43:46 +0200

    impostato la lingua, il voto e aggiunta la serie tv

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function init(){
     el:'#app',
     data: {
       query: '',
-      listaFilm:[]
+      listaFilm:[],
+      listaSerie:[]
     },
     methods:{
       cercaClick(){
@@ -22,7 +23,26 @@ function init(){
                .catch(function(){
                  console.log('error');
                })
+        this.cercaSerie()
 
+      },
+      cercaSerie(){
+        axios.get('https://api.themoviedb.org/3/search/tv',{
+          params: {
+            'api_key': 'e99307154c6dfb0b4750f6603256716d',
+            'query': this.query,
+          }})
+          .then(responSeries =>{
+            console.log(responSeries.data.results);
+            this.listaSerie = responSeries.data.results
+          })
+          .catch(function(){
+            console.log('error');
+          })
+
+      },
+      calcoloVoto(voto){
+        return  (Math.round(voto/2))
       }
     }
   })
